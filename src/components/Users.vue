d26528cb3d9c5738693cd444368809126fbbe45d
feather <1029920886@qq.com>
2018-12-28 16:30:10 +0800

    修改用户管理页

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -66,7 +66,7 @@
       title="提示"
       :visible.sync="dialogVisible"
       width="30%">
-      <el-form></el-form> 
+      <el-form></el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
